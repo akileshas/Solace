2cd107c5eb194cbaf932c0ac4bcd4a519965a08d
DUDLEYDANIEL <dudleydaniel.raj2005@gmail.com>
2024-07-14 19:38:23 +0530

    new file:   Frontend/src/assets/icons.tsx 	new file:   Frontend/src/assets/icons/icon1.png 	new file:   Frontend/src/assets/icons/icon10.png 	new file:   Frontend/src/assets/icons/icon11.png 	new file:   Frontend/src/assets/icons/icon12.png 	new file:   Frontend/src/assets/icons/icon13.png 	new file:   Frontend/src/assets/icons/icon14.png 	new file:   Frontend/src/assets/icons/icon15.png 	new file:   Frontend/src/assets/icons/icon16.png 	new file:   Frontend/src/assets/icons/icon17.png 	new file:   Frontend/src/assets/icons/icon2.png 	new file:   Frontend/src/assets/icons/icon3.png 	new file:   Frontend/src/assets/icons/icon4.png 	new file:   Frontend/src/assets/icons/icon5.png 	new file:   Frontend/src/assets/icons/icon6.png 	new file:   Frontend/src/assets/icons/icon7.png 	new file:   Frontend/src/assets/icons/icon8.png 	new file:   Frontend/src/assets/icons/icon9.png 	new file:   Frontend/src/assets/images.tsx 	new file:   Frontend/src/assets/images/img1.png 	new file:   Frontend/src/assets/images/img10.png 	new file:   Frontend/src/assets/images/img11.png 	new file:   Frontend/src/assets/images/img12.png 	new file:   Frontend/src/assets/images/img13.png 	new file:   Frontend/src/assets/images/img14.png 	new file:   Frontend/src/assets/images/img15.png 	new file:   Frontend/src/assets/images/img16.png 	new file:   Frontend/src/assets/images/img17.png 	new file:   Frontend/src/assets/images/img18.png 	new file:   Frontend/src/assets/images/img19.png 	new file:   Frontend/src/assets/images/img2.png 	new file:   Frontend/src/assets/images/img20.png 	new file:   Frontend/src/assets/images/img21.png 	new file:   Frontend/src/assets/images/img22.png 	new file:   Frontend/src/assets/images/img23.png 	new file:   Frontend/src/assets/images/img24.png 	new file:   Frontend/src/assets/images/img25.png 	new file:   Frontend/src/assets/images/img26.png 	new file:   Frontend/src/assets/images/img27.png 	new file:   Frontend/src/assets/images/img28.png 	new file:   Frontend/src/assets/images/img29.png 	new file:   Frontend/src/assets/images/img3.png 	new file:   Frontend/src/assets/images/img30.png 	new file:   Frontend/src/assets/images/img31.png 	new file:   Frontend/src/assets/images/img32.png 	new file:   Frontend/src/assets/images/img33.png 	new file:   Frontend/src/assets/images/img34.png 	new file:   Frontend/src/assets/images/img35.png 	new file:   Frontend/src/assets/images/img36.png 	new file:   Frontend/src/assets/images/img4.png 	new file:   Frontend/src/assets/images/img5.png 	new file:   Frontend/src/assets/images/img6.png 	new file:   Frontend/src/assets/images/img7.png 	new file:   Frontend/src/assets/images/img8.png 	new file:   Frontend/src/assets/images/img9.png 	new file:   Frontend/src/components/form/form.css 	new file:   Frontend/src/components/form/form.tsx 	modified:   Frontend/src/data/colors.ts 	new file:   Frontend/src/pages/signin/signin.css 	new file:   Frontend/src/pages/signin/signin.tsx

---
+++ Frontend/src/assets/icons.tsx
@@ -0,0 +1,19 @@
+import icon1 from './icons/icon1.png';
+import icon2 from './icons/icon2.png';
+import icon3 from './icons/icon3.png';
+import icon4 from './icons/icon4.png';
+import icon5 from './icons/icon5.png';
+import icon6 from './icons/icon6.png';
+import icon7 from './icons/icon7.png';
+import icon8 from './icons/icon7.png';
+import icon9 from './icons/icon9.png';
+import icon10 from './icons/icon10.png';
+import icon11 from './icons/icon11.png';
+import icon12 from './icons/icon12.png';
+import icon13 from './icons/icon13.png';
+import icon14 from './icons/icon14.png';
+import icon15 from './icons/icon15.png';
+import icon16 from './icons/icon16.png';
+import icon17 from './icons/icon17.png';
+
+export { icon1, icon2 , icon3, icon4, icon5, icon6, icon7 , icon8 , icon9, icon10, icon11, icon12, icon13, icon14, icon15, icon16, icon17 };
