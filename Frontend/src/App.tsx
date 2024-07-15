a05e81625b3d2d46ef75d26d463ff3a798911974
Akilesh A S <akilesh.ml.engineer@gmail.com>
2024-07-15 19:10:10 +0530

    Update App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import SignUpForm from './components/form/SignUpform';
+
 import Home from './pages/home/home';
 
 import About from './pages/about/about';
 import NotFound from './pages/not_found/not_found';
 import './App.css';
-import SignUpForm from './components/form/SignUpform';
 
 const App = () => {
   return (
@@ -13,9 +14,11 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
           <Route path="/signup" element={<SignUpForm />} />
+          
+          <Route path="/home" element={<Home />} />
+          
+          <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
